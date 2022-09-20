a04d4289b44750b878512a924873658e546ec1ef
Acestriker <acestriker0001@icloud.com>
2022-09-19 21:29:39 -0400

    test

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
-import Sapphire from '@sapphire/framework';
-import * as dotenv from 'dotenv'; dotenv.config(); // .env File
-const client = new Sapphire.SapphireClient({ intents: [
-    'GUILDS',
-    'GUILD_MEMBERS']});
+import Sapphire from "@sapphire/framework";
+import * as dotenv from "dotenv";
+dotenv.config(); // .env File
+
+const client = new Sapphire.SapphireClient({
+  intents: ["GUILDS", "GUILD_MEMBERS"],
+});
+
 client.login(process.env.fdVerificationToken);
