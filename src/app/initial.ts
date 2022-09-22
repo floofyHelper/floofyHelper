6e2fc92d4ad92ef3581bc7142b2f3998de34a928
Acestriker <acestriker0001@icloud.com>
2022-09-21 20:52:49 -0400

    d

--- src/app/initial.ts
+++ src/app/initial.ts
@@ -1,11 +1,12 @@
 import chalk from 'chalk'
+import inquirer from 'inquirer'
 import fs from 'node:fs' // File System
 import yaml from 'js-yaml' // .yaml File
 import * as dotenv from 'dotenv'
 dotenv.config() // .env File
 
 chalk.level = 3 // Configuring Chalk
-const client = yaml.load(fs.readFileSync('../config/config.yml', 'utf8')) // Import The config.yaml File
+const client = yaml.load(fs.readFileSync('src/config/config.yml', 'utf8')) // Import The config.yaml File
 
 export const timestamp = `${
 	new Date().getMonth() + 1
@@ -20,4 +21,26 @@ export const timestamp = `${
 	)
 } */
 
-import('./fdVerification/index.js')
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			message: 'Which environment do you want to boot into?',
+			name: 'environment',
+			choices: [
+				{
+					name: 'Development',
+				},
+				{
+					name: `Production ${chalk.red(
+						'(This will boot the bot to '
+					)}${chalk.red.underline('every')} ${chalk.red('server!)')}`,
+				},
+			],
+		},
+	])
+	.then((answers) => {
+		console.log(JSON.stringify(answers, null, '  '))
+	})
+
+// import('./fdVerification/index.js')
