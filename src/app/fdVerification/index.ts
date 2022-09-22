408386bc872ea666cf6917d3252d8ee32fbccf1d
Acestriker <acestriker0001@icloud.com>
2022-09-22 14:48:34 -0400

    woah :o

--- src/app/fdVerification/index.ts
+++ src/app/fdVerification/index.ts
@@ -7,11 +7,24 @@ console.log(
 )
 
 import Sapphire from '@sapphire/framework' // Discord API
+import fs from 'node:fs' // File System
+import path from 'node:path'
 import * as dotenv from 'dotenv' // .env File
 dotenv.config()
 
 const client = new Sapphire.SapphireClient({
-	intents: ['Guilds', 'GuildMembers'],
+	intents: ['GUILDS', 'GUILD_MEMBERS'],
 })
+
+/* client.once('ready', () => {
+	console.log(
+		chalk.underline.magenta('Startup'),
+		chalk.green(`${client.user.tag} is logged in`)
+	)
+	/* client.user.setPresence({
+		activities: [{ name: `${client.guild.memberCount} users`, type: Discord.ActivityType.Watching }],
+		status: 'online',
+	})
+}) */
 
 client.login(process.env.fdVerificationToken)
--- src/app/initial.ts
+++ src/app/initial.ts
@@ -3,7 +3,8 @@ import inquirer from 'inquirer'
 import fs from 'node:fs' // File System
 import yaml from 'js-yaml' // .yaml File
 import * as dotenv from 'dotenv'
-dotenv.config() // .env File
+import { err } from '@sapphire/framework'
+//dotenv.config() // .env File
 
 chalk.level = 3 // Configuring Chalk
 const client = yaml.load(fs.readFileSync('src/config/config.yml', 'utf8')) // Import The config.yaml File
@@ -13,13 +14,6 @@ export const timestamp = `${
 }-${new Date().getDate()}-${new Date().getFullYear()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
 
 // -------------------------------------------------------------------------------
-
-/* if (client === 'stable') {
-	console.log(
-		chalk.underline.magenta('Startup'),
-		'Config file shows "stable", searching for all production bots'
-	)
-} */
 
 inquirer
 	.prompt([
@@ -32,17 +26,41 @@ inquirer
 					name: 'Development',
 				},
 				{
-					name: `Production ${chalk.red(
-						'(This will boot the bot to'
-					)} ${chalk.red.underline('every')} ${chalk.red(
-						'server!)'
-					)}`,
+					name: 'Production',
 				},
 			],
 		},
 	])
 	.then((answers) => {
-		console.log(JSON.stringify(answers, null, '  '))
+		if (answers.environment === 'Development') {
+			dotenv.config({ path: '.env' })
+			import('./fdVerification/index.js')
+		} else if (answers.environment === 'Production') {
+			inquirer
+				.prompt([
+					{
+						type: 'list',
+						message: `Are you sure? ${chalk.red(
+							'(This will boot the bot to'
+						)} ${chalk.red.underline('every')} ${chalk.red(
+							'server!)'
+						)}`,
+						name: 'production',
+						choices: [
+							{
+								name: 'No',
+							},
+							{
+								name: 'Yes',
+							},
+						],
+					},
+				])
+				.then((answers) => {
+					if (answers.production === 'Yes') {
+						dotenv.config({ path: '.env.production' })
+						import('./fdVerification/index.js')
+					}
+				})
+		}
 	})
-
-// import('./fdVerification/index.js')
